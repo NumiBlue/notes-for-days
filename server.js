1df4600d5725c9dc9542db47d8e3d8a1a901a763
NumiBlue <thekindlyblog@gmail.com>
2022-03-19 00:09:44 -0500

    db.json

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = 3002;
 
-app.listen(3002, () => {
-    console.log(`API server now on port 30012`);
+//serving static assets
+app.use(express.static('public'))
+//app.get('/api/notation', (req, res) => {
+  //  res.sendFile(path.join(__dirname, 'index.html'));
+  //});
+console.log(path.join(__dirname, 'index.html'));
+app.listen(port, () => {
+    console.log(`API server now on port 3002!`);
   });
