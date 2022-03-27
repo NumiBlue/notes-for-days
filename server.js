4b9a32adba4e8ad5523f9019aad046373f0a82d8
NumiBlue <thekindlyblog@gmail.com>
2022-03-27 18:32:05 -0500

    notes html

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const port = 3002;
 
 //Returns object/key pair of extended/true
 app.use(express(urlencoded( { extended:true})));
+//App utilizes express.json
+app,use(express.json());
 //serving static assets
 app.use(express.static('public'))
-//app.get('/api/notation', (req, res) => {
-  //  res.sendFile(path.join(__dirname, 'index.html'));
-  //});
+
+//incorporate notes html
+app.get('/note', (req, res) => {
+  res.sendFile(__dirname + 'public/notes.html');
+});
+
 console.log(path.join(__dirname, 'index.html'));
 app.listen(port, () => {
     console.log(`API server now on port 3002!`);
