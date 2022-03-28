697d7da8009252097c1ba89e4c908def8f8e3703
NumiBlue <thekindlyblog@gmail.com>
2022-03-28 11:59:55 -0500

    db.json

--- server.js
+++ server.js
@@ -1,31 +1,61 @@
 const express = require('express');
+const res = require('express/lib/response');
 const path = require('path');
 const app = express();
-const port = 3002;
+const PORT = 3000;
 
 //Returns object/key pair of extended/true
-app.use(express(urlencoded( { extended:true})));
+app.use(express.urlencoded({ extended: true }));
 //App utilizes express.json
-app,use(express.json());
+app.use(express.json());
 //serving static assets
 app.use(express.static('public'));
 
+function createNewNote(body, notesArray) {
+  console.log(body);
+  // our function's main code will go here!
+
+  // return finished code to post route for response
+  return body;
+}
+
+
 //incorporate notes html
 app.get('/note', (req, res) => {
-  res.sendFile(__dirname + 'public/notes.html');
+  res.sendFile(__dirname + '/public/notes.html');
 });
 
 //utilizes API routes
-const apiRoutes = require('./routes/notes')
-app.use('/api', apiRoutes);
+//const apiRoutes = require('./routes/notes')
+//app.use('/api', apiRoutes);
+function createNewNote(body, notesArray) {
+  const notes = body;
+  notesArray.push(notes);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return notes;
+}
 
 //includes index.html
-app.get('*', (req, res) => {
+app.get('/index.html', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 });
 
+//app.post('/note', (req, res) => {
+ // res.
+//
+app.post('/note', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  res.json(req.body);
+});
+
+//app.delete
 
 //If app listens, log Port message
-app.listen(port, () => {
-    console.log(`API server now on PORT: ${PORT}}`);
+app.listen(PORT, () => {
+    console.log(`API server now on PORT: ${PORT}`);
   });
